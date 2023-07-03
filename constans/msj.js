6dde65c329fcbab6d0a826deccbb1adf0c8ae97b
= <=>
2023-07-03 10:36:53 -0600

    Mejoras

--- constans/msj.js
+++ constans/msj.js
@@ -1,5 +1,5 @@
 export const msj = {
-    msjPrincipal: `
+  msjPrincipal: `
     Bienvenido a CAPISTRANO. Sabor que premia 🏆
     \nY para participar, solo deberás
     👉 Registrarte y
@@ -7,18 +7,31 @@ export const msj = {
     \nRecuerda que para registrarte solicitaremos tu *Número de identificador de cliente*, Nombre, Apellido, Correo electrónico, Estado de la república
     \n¿Estas listo(a) para iniciar? 🤔
     \n*¡Solo escribe el número 1 para iniciar tu registro!*`,
-    msjPrincipal2:``,
-    msjErrRegistro: `
+  msjPrincipal2: ``,
+  msjErrRegistro: `
     Estimado participante. 😣
     \nLe informamos que el número de identificador de cliente *${idCliente}*, proporcionado no está registrado en nuestra base de datos, por lo que le invitamos a rectificar el número correcto y volver a empezar su proceso de registro.
     \nEn caso de que sea un error, le invitamos a contactar a nuestro equipo de atención a clientes enviando un correo electrónico a xxxx@capistrano.com para que su número sea dado de alta.
     \n*Sí ingresaste algún dato incorrecto puedes volver a empezar tu registro escribindo el número 1 en cualquier momento.*`,
-    msjErrRegistro2: `
+  msjErrRegistro2: `
     Estimado participante. 😣
     \nLe informamos que según nuestra base de datos el registro a este consurso ya fue realizado previamente con la siguente información.
     \n📅 Fecha de registro: *##/##/20##*
     \n📱 Número telefónico con terminación: *XX XXXX XX07*
     \n📧 Correo electrónico: *xxxx@gmail.com*
     \nEn caso de que sea un error, le invitamos a contactar a nuestro equipo de atención a clientes enviando un correo electrónico a xxxx@capistrano.com para que su número sea dado de alta.
-    \n*Sí ingresaste algún dato incorrecto puedes volver a empezar tu registro escribindo el número 1 en cualquier momento.*`
-}    
+    \n*Sí ingresaste algún dato incorrecto puedes volver a empezar tu registro escribindo el número 1 en cualquier momento.*`,
+};
+
+let datosSesion = {
+  nombre: "",
+  apellido: "",
+};
+
+export const setData = (obj) => {
+  datosSesion.nombre = obj.nombre;
+};
+
+export const getData = (obj) => {
+  return datosSesion;
+};
--- controllers/main.js
+++ controllers/main.js
@@ -1,23 +1,25 @@
-const { addKeyword } = require('@bot-whatsapp/bot')
-const flowPrincipal = addKeyword(['hola', 'ole', 'alo'])
-    .addAnswer('🚩 CAPISTRANO. Sabor que premia 🚩',
-        null,
-        async (ctx,{flowDynamic}) => {
-            // if (ctx.from == '5215549434127')
-            //     return endFlow({
-            //         body: `¡UPS! Tu numero distribuidor *${numeroDistribuidor}* no es válido, por favor comunícate al 5512341234`,    // Aquí terminamos el flow si la condicion se comple        
-            //     })
-            return flowDynamic(
-                `Bienvenido 🏆
+const { addKeyword, addChild } = require("@bot-whatsapp/bot");
+const flowRegistro = require("./registro");
+const flowPrincipal = addKeyword(["hola", "ole", "alo"]).addAnswer(
+  "🚩 CAPISTRANO. Sabor que premia 🚩",
+  null,
+  async (ctx, { flowDynamic }) => {
+    // if (ctx.from == '5215549434127')
+    //     return endFlow({
+    //         body: `¡UPS! Tu numero distribuidor *${numeroDistribuidor}* no es válido, por favor comunícate al 5512341234`,    // Aquí terminamos el flow si la condicion se comple
+    //     })
+    return flowDynamic(
+      `Bienvenido esta es tu oportunidad para ganar 🏆 miles de pesos en premios.
                 \nY para participar, solo deberás
                 
                 👉 Registrarte y
                 👉 Superar tus compras semanales
                 \nRecuerda que para registrarte solicitaremos tu *Número de identificador de cliente*, Nombre, Apellido, Correo electrónico, Estado de la república
                 \n¿Estas listo(a) para iniciar? 🤔
-                \n*¡Solo escribe el número 1 para iniciar tu registro!*`
-            )
-        },[]
-    )
+                \n*¡Solo escribe *Iniciar* para comenzar tu registro!*`
+    );
+  },
+  [flowRegistro]
+);
 
-module.exports = flowPrincipal
+module.exports = flowPrincipal;
