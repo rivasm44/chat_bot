25b2c2eb96db98af86fb25162f793b4b4172dbff
Nestor Resendiz <nestor.resendiz10@gmail.com>
2023-07-08 19:48:27 -0600

    Ajuste bienvenida

--- controllers/main.js
+++ controllers/main.js
@@ -5,25 +5,28 @@ const {
   getUserActive,
 } = require("../reposotori/firestore");
 
+const flowInicio = addKeyword('binvenida', { sensitive: true })
+  .addAnswer(['Bienvenido esta es tu oportunidad para ganar 🏆 miles de pesos en premios.'
+    , 'Y para participar, solo deberás'
+    , '👉 Registrarte y👉 Superar tus compras semanales'
+    , 'Recuerda que para registrarte solicitaremos tu *Número de identificador de cliente*, Nombre, Apellido, Correo electrónico, Estado de la república'
+    , '¿Estas listo(a) para iniciar? 🤔'])
+  .addAnswer(
+    '¡Solo escribe la palabra *registro* para empezar tu Registro! 😎'
+    , { capture: true }, (ctx, { fallBack }) => {
+      if (ctx.body != 'registro' && ctx.body != 'Registro' && ctx.body != 'REGISTRO') return fallBack()
+    }, [flowRegistro]
+  );
+
 const flowPrincipal = addKeyword(EVENTS.WELCOME).addAnswer(
   "🚩 CAPISTRANO. Sabor que premia 🚩",
   null,
   async (ctx, { gotoFlow }) => {
-    const userActive = await getUserActive("TELEFONO",ctx.from);
+    const userActive = await getUserActive("TELEFONO", ctx.from);
     console.log("Usuario activo ---->", userActive);
-    
-      console.log("userActive.activo ---->", userActive.activo);
+    console.log("userActive.activo ---->", userActive.activo);
     if (userActive.activo) await gotoFlow(flowMenu)
-  }).addAnswer(
-    `Bienvenido esta es tu oportunidad para ganar 🏆 miles de pesos en premios.
-      \nY para participar, solo deberás
-      \n👉 Registrarte y\n👉 Superar tus compras semanales
-      \nRecuerda que para registrarte solicitaremos tu *Número de identificador de cliente*, Nombre, Apellido, Correo electrónico, Estado de la república
-      \n¿Estas listo(a) para iniciar? 🤔
-      \n¡Solo escribe la palabra *registro* para empezar tu Registro!`
-      ,{ capture: true },(ctx,{fallBack})=> {
-        if (ctx.body != 'registro' && ctx.body != 'Registro' && ctx.body != 'REGISTRO') return fallBack()
-      },[flowRegistro, flowMenu]
-  );
+    else gotoFlow(flowInicio)
+  }, [flowInicio, flowMenu])
 
 module.exports = flowPrincipal;
