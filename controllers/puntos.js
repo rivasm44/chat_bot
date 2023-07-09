73f075740c2b307519bbf6c3c1e7a273b9c709c2
= <=>
2023-07-09 12:16:51 -0600

    Ajustes en el orden de metricas...

--- controllers/puntos.js
+++ controllers/puntos.js
@@ -12,10 +12,10 @@ const flowMenuOp3 = addKeyword(["3", "tres", "Tres"], {
   const { registro } = await validateId(user.ID);
   const { metricas } = await getAccumulated(user.ID);
 
-  if (registro["OBJ CREC"] < metricas.CREC) {
+  if (metricas.CREC < registro["OBJ CREC"]) {
     return flowDynamic(`*${user.NOMBRES} ${user.APELLIDOS}*
                 \nTe compartimos tu alcance de la semana número *${metricas.SEMANA}* en CAPISTRANO. Sabor que premia:
-                \n🚀 *KPS* = ${metricas.KPS}\n🚀 *$PS* = ${metricas.$PS}\n🚀 *Objetivo de crecimiento* = ${registro["OBJ CREC"]}\n🚀 *Crecimiento* = ${metricas.CREC}\n🚀 *Total de Puntos actuales* = ${metricas.PUNTOS}
+                \n🚀 *Objetivo de crecimiento* = ${registro["OBJ CREC"]}\n🚀 *Crecimiento* = ${metricas.CREC}\n🚀 *KPS* = ${metricas.KPS}\n🚀 *$PS* = ${metricas.$PS}\n🚀 *Total de Puntos actuales* = ${metricas.PUNTOS}
                 \n🎯 Te invitamos a que realices una nueva compra, sumar puntos y aumentar tus oportunidades para ganar.\n🎯 No dejes de participar y preparate para ganar con CAPISTRANO. Sabor que premia.
                 \n¿Deseas conocer más? 🤔
                 \nEscribe *menu* para ingresar a tú menú principal.`);
@@ -23,14 +23,14 @@ const flowMenuOp3 = addKeyword(["3", "tres", "Tres"], {
   if (registro["OBJ CREC"] === metricas.CREC) {
     return flowDynamic(`*${user.NOMBRES} ${user.APELLIDOS}*
                 \nQueremos informate que vas muy bien en tus compras con gusto te compartimos tu alcance hasta la fecha:
-                \n🚀 *KPS* = ${metricas.KPS}\n🚀 *$PS* = ${metricas.$PS}\n🚀 *Objetivo de crecimiento* = ${registro["OBJ CREC"]}\n🚀 *Crecimiento* = ${metricas.CREC}\n🚀 *Total de Puntos actuales* = ${metricas.PUNTOS}
+                \n🚀 *Objetivo de crecimiento* = ${registro["OBJ CREC"]}\n🚀 *Crecimiento* = ${metricas.CREC}\n🚀 *KPS* = ${metricas.KPS}\n🚀 *$PS* = ${metricas.$PS}\n🚀 *Total de Puntos actuales* = ${metricas.PUNTOS}
                 \n🎯 Te invitamos a que realices una nueva compra, sumar puntos y aumentar tus oportunidades para ganar.\n🎯 No dejes de participar y preparate para ganar con CAPISTRANO. Sabor que premia.
                 \n¿Deseas conocer más? 🤔
                 \nEscribe *menu* para ingresar a tú menú principal.`);
   } else {
     return flowDynamic(`!WOW! *${user.NOMBRES} ${user.APELLIDOS}* 🤩
                 \nTu puntaje en CAPISTRANO. Sabor que premia va increible 🙌 
-                \n🚀 *KPS* = ${metricas.KPS}\n🚀 *$PS* = ${metricas.$PS}\n🚀 *Objetivo de crecimiento* = ${registro["OBJ CREC"]}\n🚀 *Crecimiento* = ${metricas.CREC}\n🚀 *Total de Puntos actuales* = ${metricas.PUNTOS}
+                \n🚀 *Objetivo de crecimiento* = ${registro["OBJ CREC"]}\n🚀 *Crecimiento* = ${metricas.CREC}\n🚀 *KPS* = ${metricas.KPS}\n🚀 *$PS* = ${metricas.$PS}\n🚀 *Total de Puntos actuales* = ${metricas.PUNTOS}
                 \n🎯 Te invitamos a que realices una nueva compra, sumar puntos y aumentar tus oportunidades para ganar.\n🎯 No dejes de participar y preparate para ganar con CAPISTRANO. Sabor que premia.
                 \n¿Deseas conocer más? 🤔
                 \nEscribe *menu* para ingresar a tú menú principal.`);
