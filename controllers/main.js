aa39993beefa80e0bf1b763c16fbc0c1b4500f69
Nestor Resendiz <nestor.resendiz10@gmail.com>
2023-07-09 08:40:11 -0600

    fix registro

--- controllers/main.js
+++ controllers/main.js
@@ -5,30 +5,26 @@ const {
   getUserActive,
 } = require("../reposotori/firestore");
 
-const flowInicio = addKeyword('binvenida', { sensitive: true })
-  .addAnswer(['Bienvenido esta es tu oportunidad para ganar 🏆 miles de pesos en premios.'
-    , '\nY para participar, solo deberás'
-    ,'\n👉 Registrarte y'
-    ,'👉 Superar tus compras semanales'
-    , '\nRecuerda que para registrarte solicitaremos tu *Número de identificador de cliente*, Nombre, Apellido, Correo electrónico, Estado de la república'
-    , '\n¿Estas listo(a) para iniciar? 🤔'])
-  .addAnswer(
-    '¡Solo escribe la palabra *registro* para empezar tu Registro! 😎'
-    , { capture: true }, (ctx, { fallBack }) => {
-      if (ctx.body != 'registro' && ctx.body != 'Registro' && ctx.body != 'REGISTRO') return fallBack()
-    }, [flowRegistro]
-  );
 
 const flowPrincipal = addKeyword(EVENTS.WELCOME).addAnswer(
-  "CAPISTRANO. Sabor que premia 🎁",
+  "🚩 CAPISTRANO. Sabor que premia 🚩",
   null,
   async (ctx, { gotoFlow }) => {
-    const userActive = await getUserActive("TELEFONO", ctx.from);
-    // const userActive = await getUserActive("TELEFONO", '5215549434188');
+    const userActive = await getUserActive("TELEFONO",ctx.from);
     console.log("Usuario activo ---->", userActive);
-    console.log("userActive.activo ---->", userActive.activo);
+    
+      console.log("userActive.activo ---->", userActive.activo);
     if (userActive.activo) await gotoFlow(flowMenu)
-    else gotoFlow(flowInicio)
-  }, [flowInicio, flowMenu])
+  }).addAnswer(
+    `Bienvenido esta es tu oportunidad para ganar 🏆 miles de pesos en premios.
+      \nY para participar, solo deberás
+      \n👉 Registrarte y\n👉 Superar tus compras semanales
+      \nRecuerda que para registrarte solicitaremos tu *Número de identificador de cliente*, Nombre, Apellido, Correo electrónico, Estado de la república
+      \n¿Estas listo(a) para iniciar? 🤔
+      \n¡Solo escribe la palabra *registro* para empezar tu Registro!`
+      ,{ capture: true },(ctx,{fallBack})=> {
+        if (ctx.body.toUpperCase() != 'REGISTRO' && ctx.body.toUpperCase() != 'REGISTRATE' && ctx.body.toUpperCase() != 'REGISTRO ' && ctx.body.toUpperCase() != 'REGISTRATE ') return fallBack()
+      },[flowRegistro, flowMenu]
+  );
 
 module.exports = flowPrincipal;
