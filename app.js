4ce062c06973eb978d2697a065e97ec53732f739
= <=>
2023-07-16 21:40:33 -0600

    Empate con archivos gcp...

--- app.js
+++ app.js
@@ -7,36 +7,35 @@ const { sendMessages } = require("./controllers/sendNotifications");
 const express = require("express");
 const app = express();
 try {
-  const adapterDB = new MockAdapter();
-  const adapterFlow = createFlow([FlowMain]);
-  const adapterProvider = createProvider(BaileysProvider);
-  app.get("/inicio", (req, res) => {
-    res.send("CAPISTRANO. Sabor que premia!");
-  });
-
-  app.get("/send-message-bot", async (req, res) => {
-    console.log(req);
-    sendMessages(req, res, adapterProvider);
-  });
-  const PORT = parseInt(process.env.PORT) || 5000;
-  app.listen(PORT, () =>
-    console.log(`Servidor de notificaciones en el puerto:${PORT}`)
-  );
-
-  const main = async () => {
-    createBot({
-      flow: adapterFlow,
-      provider: adapterProvider,
-      database: adapterDB,
+    const adapterDB = new MockAdapter();
+    const adapterFlow = createFlow([FlowMain]);
+    const adapterProvider = createProvider(BaileysProvider);
+    app.get("/inicio", (req, res) => {
+        res.send("CAPISTRANO. Sabor que premia!");
     });
 
-    const PORT_CAT = parseInt(process.env.PORT) || 8080;
-    console.log(`En el puerto ----> ${PORT_CAT}`);
-    QRPortalWeb({ port: PORT_CAT });
-  };
+    app.get("/send-message-bot", async (req, res) => {
+        console.log(req);
+        sendMessages(req, res, adapterProvider);
+    });
+    const PORT = 5000;
+    app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
 
-  main();
+    const main = async () => {
+
+        createBot({
+            flow: adapterFlow,
+            provider: adapterProvider,
+            database: adapterDB,
+        });
+
+        const PORT_CAT = parseInt(process.env.PORT) || 8080;
+        console.log(`En el puerto ----> ${PORT_CAT}`);
+        QRPortalWeb({ port: PORT_CAT });
+    };
+
+    main();
 } catch (error) {
-  console.log("Error ------>");
-  console.log(error);
+    console.log("Error ------>");
+    console.log(error);
 }
